eaa77f6380ff492019698641a49868bf378a69bd
lyalka69 <l.y.a.l.k.a7.12.7@gmail.com>
2024-02-23 19:28:18 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktikum6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Составить программный код решения квадратного уравнения ax2+bx+c=0 двумя способами:
+            //с использованием неполного условного оператора, вложенного условного оператора.
+
+            //Console.Write("Введите a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите b: ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("Введите c: ");
+            //int c = int.Parse(Console.ReadLine());
+            //double D, x1, x2;
+
+
+            //D = Math.Pow(b, 2) - 4 * a * c;
+            //if (D < 0)
+            //{
+            //    Console.WriteLine("квадратное уравнение не имеет корней");
+            //}
+            //if (D == 0)
+            //{
+            //    Console.WriteLine($"квадратное уравнение имеет один действительный корень \nx1 = x2 = {(-b + Math.Sqrt(D))/(2*a)}");
+            //}
+            //if (D > 0) 
+            //{
+            //    Console.WriteLine($"квадратное уравнение имеет два действительных корня \nx1 = {(-b + Math.Sqrt(D))/(2*a)} \nx2 = {(-b - Math.Sqrt(D)) / (2 * a)}");
+            //}
+            //Console.ReadKey();
+
+
+            //D = Math.Pow(b, 2) - 4 * a * c;
+            //if (D < 0)
+            //{
+            //    Console.WriteLine("квадратное уравнение не имеет корней");
+            //}
+            //else if (D == 0)
+            //{
+            //    Console.WriteLine($"квадратное уравнение имеет один действительный корень \nx1 = x2 = {(-b + Math.Sqrt(D)) / (2 * a)}");
+            //}
+            //else if (D > 0)
+            //{
+            //    Console.WriteLine($"квадратное уравнение имеет два действительных корня \nx1 = {(-b + Math.Sqrt(D)) / (2 * a)} \nx2 = {(-b - Math.Sqrt(D)) / (2 * a)}");
+            //}
+            //Console.ReadKey();
+
+
+            //Составить програмный код вычисления площади треугольникасо сторонами a,b,c если он существует,
+            //в противном случае - вывести сообщение о том что треугольника нет.
+
+            //Console.Write("Введите длину триугольника a: ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("Введите длину триугольника b: ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("Введите длину триугольника c: ");
+            //double c = double.Parse(Console.ReadLine());
+            //double  S;
+           
+            //S = 0.5 * a * b - Math.Sin(a);
+            //if (a + b > c & b + c > a & c + a > b)
+            //    Console.WriteLine($"S треугольника равна: {S:f2}");
+            //else
+            //    Console.WriteLine("треугольника нет");
+            //Console.ReadKey();
+
+
+        }
+    }
+}
